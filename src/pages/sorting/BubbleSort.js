9d047952b31b9a6c209a3952aa3e46f14f676577
piupaulgit <contactpiu619@gmail.com>
2024-05-21 21:11:07 +0530

    bubble sort

--- src/pages/sorting/BubbleSort.js
+++ src/pages/sorting/BubbleSort.js
@@ -32,6 +32,50 @@ const BubbleSort = () => {
 
         <div className="my-8">
           <h3 className="text-xl pb-2">Full Implementation: </h3>
+          <pre className="bg-white p-4 rounded shadow-md overflow-x-auto mt-2">
+            <code>
+              {`const bubbleSort = (arr) => {
+                const n = arr.length;
+                for (let i = arr.length-1; i >= 0; i--){
+                    let smallest = i;
+                    for (let j = i-1; j >= 0; j--) {
+                        if(arr[smallest] < arr[j]){
+                            [arr[smallest], arr[j]] = [arr[j], arr[smallest]]
+                        } else{
+                            smallest = j;
+                        }
+                    }
+                }
+                return arr;
+            }
+            
+            bubbleSort(["card", "flight", "abs", "night"])
+            //output: ['abs', 'card', 'flight', 'night']`}
+            </code>
+          </pre>
+        </div>
+
+        <div className="my-8">
+          <h3 className="text-xl pb-2">Characteristics: </h3>
+          <ul>
+            <li>
+              <b>Time Complexity:</b> O(n^2) in the worst and average cases,
+              where n is the number of items being sorted.
+            </li>
+            <li>
+              <b>Space Complexity:</b> O(1) because it is an in-place sorting
+              algorithm.
+            </li>
+          </ul>
+        </div>
+
+        <div className="my-8">
+          <p>
+            Bubble Sort is an intuitive but inefficient sorting algorithm for
+            large datasets due to its O(n^2) time complexity. It performs well
+            on small or nearly sorted datasets and is easy to implement and
+            understand.
+          </p>
         </div>
       </div>
     </div>
